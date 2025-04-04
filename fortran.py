5bfa3a6fdbe607a81fe6b8599c7f33cedc8c0e6d
Martin Böschen <martinboeschen@hotmail.com>
2025-04-04 11:01:41 +0000

    Formeln

--- fortran.py
+++ fortran.py
@@ -2,22 +2,16 @@
 def sgerg88(X2,X3,HS, RM, X5, P,TC, Z,D):
     """Placeholder function for the sgerg88 algorithm.	
     Args:
-        X2 (float): Input parameter X2, description needed.
-        X3 (float): Input parameter X3, description needed.
-        HS (float): Input parameter HS, description needed.
-        RM (float): Input parameter RM, description needed.
-        X5 (float): Input parameter X5, description needed.
+        X2 (float): Return parameter, Stoffanteil nitroge
+        X3 (float): Stoffanteil co2
+        HS (float): Caloric value i MJ/m3
+        RM (float): Realtive density
+        X5 (float): Stoffanteil h2
         P (float): Pressure in bar
-        TC (float): Temepratur in Celcsius
-        Z (float): Compressibility factor of the gas mixture.
-        D (float): Density of the gas mixture.
-
+        TC (float): Temperature in Celcsius
+        Z (float): return parameter, Compressibility factor of the gas mixture.
+        D (float): return parameter, molare density
     
-
-    Placeholder function for the sgerg88 algorithm.
-    This function is currently not implemented. The sgerg88 algorithm is 
-    typically used for calculating properties of gas mixtures based on 
-    the GERG-88 equation of state.
     Returns:
         None
     """
